d1ca35ca9d7f0f393eef8d224ff2b7f79dc4702f
César Enrrique Carranza Jordan <enrrique.carranza@pucp.edu.pe>
2026-04-21 13:43:31 -0500

    Refactor Paciente class by removing unused attributes

--- Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Paciente.java
+++ Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Paciente.java
@@ -2,96 +2,59 @@ package pe.edu.pucp.kirusmile.models;
 
 import java.util.Date;
 
-public class Paciente extends Persona {
+public class Paciente extends Persona{
+	private int id;
+	private String estado;//ACTIVO(ES PACIENTE REGISTRADO), INACTIVO(NO REGISTRADO), BETADO (NO ESTA PERMITIDO)
+	private boolean tieneSeguro;
+	private boolean desactivado;
+	
+	
+	public Paciente(String dni,String nombres,String apellidoPaterno, String apellidoMaterno,Date fechaNacimiento,
+					String telefono,String correo,int id,String estado, boolean tieneSeguro,boolean desactivado){
+		super(dni,nombres,apellidoPaterno,apellidoMaterno,fechaNacimiento,telefono,correo);
+		this.id=id;
+		this.estado=estado;
+		this.tieneSeguro=tieneSeguro;
+		this.desactivado=desactivado;
+	}
 
-    // --- ATRIBUTOS (NTS 139 - Num. 6.1.2 y Anexo 2) ---
-    private String grupoSanguineo;
-    private String factorRh;
-    private String gradoInstruccion;
-    private String ocupacion;
-    private String etnia;
-    private HistorialMedico historial; // Relación 1 a 1 con su historial
+	public int getId() {
+		return id;
+	}
 
-    // --- GETTERS Y SETTERS INTERCALADOS ---
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getGrupoSanguineo() {
-        return grupoSanguineo;
+	public boolean getDesactivado() {
+		return desactivado;
+	}
+
+	public void setDesactivado(boolean desactivado) {
+		this.desactivado = desactivado;
+	}
+
+	public String getEstado() {
+        return estado;
     }
 
-    public void setGrupoSanguineo(String grupoSanguineo) {
-        this.grupoSanguineo = grupoSanguineo;
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+	
+	
+
+	public boolean getTieneSeguro() {
+        return tieneSeguro;
     }
 
-    public String getFactorRh() {
-        return factorRh;
+    public void setTieneSeguro(boolean tieneSeguro) {
+        this.tieneSeguro = tieneSeguro;
     }
 
-    public void setFactorRh(String factorRh) {
-        this.factorRh = factorRh;
-    }
-
-    public String getGradoInstruccion() {
-        return gradoInstruccion;
-    }
-
-    public void setGradoInstruccion(String gradoInstruccion) {
-        this.gradoInstruccion = gradoInstruccion;
-    }
-
-    public String getOcupacion() {
-        return ocupacion;
-    }
-
-    public void setOcupacion(String ocupacion) {
-        this.ocupacion = ocupacion;
-    }
-
-    public String getEtnia() {
-        return etnia;
-    }
-
-    public void setEtnia(String etnia) {
-        this.etnia = etnia;
-    }
-
-    public HistorialMedico getHistorial() {
-        return historial;
-    }
-
-    public void setHistorial(HistorialMedico historial) {
-        this.historial = historial;
-    }
-
-    public Paciente() {
-        super();
-    }
-
-    public Paciente(String dni, String nombres, String apellidoPaterno, String apellidoMaterno, Date fechaNacimiento,
-            String telefono, String correo, String grupoSanguineo, String factorRh,
-            String gradoInstruccion, String ocupacion, String etnia) {
-        super(dni, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefono, correo); // Datos heredados de
-                                                                                                  // Persona
-        this.grupoSanguineo = grupoSanguineo;
-        this.factorRh = factorRh;
-        this.gradoInstruccion = gradoInstruccion;
-        this.ocupacion = ocupacion;
-        this.etnia = etnia;
-    }
-
-    // --- MÉTODOS ---
-
-    public void generarHistorialAutomatico() {
-        if (this.historial == null) {
-            this.historial = new HistorialMedico();
-            System.out.println("Se ha generado el Historial Clínico N° " + this.getDni());
-        }
-    }
-
-    public String obtenerNombreCompleto() {
-        return super.obtenerNombreCompleto();
-    }
-
-    public boolean tieneHistorialActivo() {
-        return this.historial != null;
-    }
+	
+	@Override
+	public void mostrarDatos(){
+		
+	}
 }
