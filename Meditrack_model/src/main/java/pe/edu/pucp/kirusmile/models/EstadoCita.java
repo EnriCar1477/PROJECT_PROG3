f3968d2ed12eb4b64e8206d8ce5fbcad794ab96e
Jair LLaiqui <a20222757@pucp.edu.pe>
2026-04-21 12:34:00 -0500

    Add files via upload

---
+++ Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/EstadoCita.java
@@ -0,0 +1,5 @@
+package pe.edu.pucp.kirusmile.models;
+
+public enum EstadoCita {
+    PROGRAMADA, CONFIRMADA, CANCELADA, ATENDIDA, NO_ASISTIO, PENDIENTE_PAGO;
+}
---
+++ Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/RolUsuario.java
@@ -0,0 +1,5 @@
+package pe.edu.pucp.kirusmile.models;
+
+public enum RolUsuario {
+    MEDICO, SECRETARIA, ADMIN;
+}
