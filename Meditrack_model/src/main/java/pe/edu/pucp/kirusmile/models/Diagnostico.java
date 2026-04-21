5c5b59940c91c79a29af01e3228dba75b684f5b2
César Enrrique Carranza Jordan <enrrique.carranza@pucp.edu.pe>
2026-04-21 11:35:51 -0500

    Update Diagnostico.java

--- Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Diagnostico.java
+++ Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Diagnostico.java
@@ -1,70 +1,49 @@
 package pe.edu.pucp.kirusmile.models;
 
+import java.time.LocalDateTime;
 import java.util.Date;
-
 public class Diagnostico{
-	private int idDiagnostico;
-	private String descripcion;
-	private String gravedad; // Podriamos crear un enum TIPO_GRAVEDAD: leve, INTERMEDIO, grave, muy grave
-	private Date fechaDiagnostico;
-	
-	// cita y diagnóstico y si debería ir aquí la cita
-	
-	public Diagnostico(int idDiagnostico,String descripcion,String gravedad,Date fechaDiagnostico,Cita cita) {
-        this.idDiagnostico=idDiagnostico;
-        this.descripcion=descripcion;
-        this.gravedad=gravedad;
-        this.fechaDiagnostico=fechaDiagnostico;
-//        this.cita=cita;
+	private String tipo;
+    private String observaciones;
+    private EnfermedadCIE10 enfermedadPrincial;
+    private LocalDateTime fechaHoraRegistro;
+
+    public Diagnostico(String tipo, String observaciones, EnfermedadCIE10 enfermedadPrincial, LocalDateTime fechaHoraRegistro) {
+        this.tipo = tipo;
+        this.observaciones = observaciones;
+        this.enfermedadPrincial = enfermedadPrincial;
+        this.fechaHoraRegistro = fechaHoraRegistro;
     }
 
-    public int getIdDiagnostico() {
-        return idDiagnostico;
+    public String getTipo() {
+        return tipo;
     }
 
-    public void setIdDiagnostico(int idDiagnostico) {
-        this.idDiagnostico = idDiagnostico;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+    public String getObservaciones() {
+        return observaciones;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
     }
 
-    public String getGravedad() {
-        return gravedad;
+    public EnfermedadCIE10 getEnfermedadPrincial() {
+        return enfermedadPrincial;
     }
 
-    public void setGravedad(String gravedad) {
-        this.gravedad = gravedad;
+    public void setEnfermedadPrincial(EnfermedadCIE10 enfermedadPrincial) {
+        this.enfermedadPrincial = enfermedadPrincial;
     }
 
-    public Date getFechaDiagnostico() {
-        return fechaDiagnostico;
+    public LocalDateTime getFechaHoraRegistro() {
+        return fechaHoraRegistro;
     }
 
-    public void setFechaDiagnostico(Date fechaDiagnostico) {
-        this.fechaDiagnostico = fechaDiagnostico;
+    public void setFechaHoraRegistro(LocalDateTime fechaHoraRegistro) {
+        this.fechaHoraRegistro = fechaHoraRegistro;
     }
-
-//    public pe.edu.pucp.kirusmile.models.Cita getCita() {
-//        return cita;
-//    }
-//
-//    public void setCita(pe.edu.pucp.kirusmile.models.Cita cita) {
-//        this.cita = cita;
-//    }
-	
-	public void agregarDescripcion(){
-		
-	}
-	
-	public void actualizarDescripcion(){
-		
-	}
-	
-	
 }
