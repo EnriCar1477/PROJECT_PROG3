2bc23633bb0066b512828153229e9fdc48791841
César Enrrique Carranza Jordan <enrrique.carranza@pucp.edu.pe>
2026-04-21 13:52:44 -0500

    Change id type from int to Integer in Paciente

--- Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Paciente.java
+++ Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Paciente.java
@@ -3,14 +3,14 @@ package pe.edu.pucp.kirusmile.models;
 import java.util.Date;
 
 public class Paciente extends Persona{
-	private int id;
+	private Integer id;
 	private String estado;//ACTIVO(ES PACIENTE REGISTRADO), INACTIVO(NO REGISTRADO), BETADO (NO ESTA PERMITIDO)
 	private boolean tieneSeguro;
 	private boolean desactivado;
 	
 	
 	public Paciente(String dni,String nombres,String apellidoPaterno, String apellidoMaterno,Date fechaNacimiento,
-					String telefono,String correo,int id,String estado, boolean tieneSeguro,boolean desactivado){
+					String telefono,String correo,Integer id,String estado, boolean tieneSeguro,boolean desactivado){
 		super(dni,nombres,apellidoPaterno,apellidoMaterno,fechaNacimiento,telefono,correo);
 		this.id=id;
 		this.estado=estado;
@@ -18,11 +18,11 @@ public class Paciente extends Persona{
 		this.desactivado=desactivado;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
