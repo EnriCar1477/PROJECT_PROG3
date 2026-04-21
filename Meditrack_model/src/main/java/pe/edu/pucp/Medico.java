cdd315e11937655c449c0996282dcb63bc2ed486
César Enrrique Carranza Jordan <enrrique.carranza@pucp.edu.pe>
2026-04-21 12:16:08 -0500

    Update and rename Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Doctor.java to Meditrack_model/src/main/java/pe/edu/pucp/Medico.java

---
+++ Meditrack_model/src/main/java/pe/edu/pucp/Medico.java
@@ -0,0 +1,83 @@
+package pe.edu.pucp.kirusmile.models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Medico extends Persona{
+	private String cmp;
+	private String rne;
+	private Especialidad especialidad;
+	private Date fechaIngreso;
+	private String firmaDigital;
+	private List<HorarioDisponibilidad> listaHorarios;
+
+
+	public Medico(String dni, String nombres, String apellidoPaterno, String apellidoMaterno, Date fechaNacimiento,
+				  String telefono, String correo, String cmp, String rne, Especialidad especialidad, Date fechaIngreso,
+				  String firmaDigital, List<HorarioDisponibilidad> listaHorarios) {
+		super(dni, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefono, correo);
+		this.cmp = cmp;
+		this.rne = rne;
+		this.especialidad = especialidad;
+		this.fechaIngreso = fechaIngreso;
+		this.firmaDigital = firmaDigital;
+		this.listaHorarios = listaHorarios;
+	}
+
+	public String getCmp() {
+		return cmp;
+	}
+
+	public void setCmp(String cmp) {
+		this.cmp = cmp;
+	}
+
+	public String getRne() {
+		return rne;
+	}
+
+	public void setRne(String rne) {
+		this.rne = rne;
+	}
+
+	public Especialidad getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(Especialidad especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public Date getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(Date fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public String getFirmaDigital() {
+		return firmaDigital;
+	}
+
+	public void setFirmaDigital(String firmaDigital) {
+		this.firmaDigital = firmaDigital;
+	}
+
+	public List<HorarioDisponibilidad> getListaHorarios() {
+		return listaHorarios;
+	}
+
+	public void setListaHorarios(List<HorarioDisponibilidad> listaHorarios) {
+		this.listaHorarios = listaHorarios;
+	}
+	public void registrarCredenciales(String cmp,String rne,Especialidad especialidad){
+
+	}
+	/*public boolean validarEspecialidad(){}*/
+	public void actualizarFirmarDigital(String nuevoToken){
+		this.setFirmaDigital(firmaDigital);
+	}
+
+	/*public double obtenerTarifaConsulta(){}*/
+}
