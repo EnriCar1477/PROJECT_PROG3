cf59793be8a71374eaa185ef45051273fd50f93d
César Enrrique Carranza Jordan <enrrique.carranza@pucp.edu.pe>
2026-04-22 09:48:08 -0500

    Update Tratamiento.java

--- Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Tratamiento.java
+++ Meditrack_model/src/main/java/pe/edu/pucp/kirusmile/models/Tratamiento.java
@@ -3,29 +3,18 @@ package pe.edu.pucp.kirusmile.models;
 import java.util.Date;
 
 public class Tratamiento{//TRATAMIENTO
-
-    
-
-    private Integer IdTratamiento;
     private TipoTratamiento tipo;
     private String indicaciones;
     private Date fechaInicio;
     private Date fechaFin;
     private boolean desactivado;
 
-    public Tratamiento(Integer idTratamiendo,TipoTratamiento tipo, String indicaciones, Date fechaInicio, Date fechaFin,boolean desactivado) {
+    public Tratamiento(TipoTratamiento tipo, String indicaciones, Date fechaInicio, Date fechaFin,boolean desactivado) {
         this.tipo = tipo;
         this.indicaciones = indicaciones;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
         this.desactivado=desactivado;
-    }
-    public Integer getIdTratamiento() {
-        return IdTratamiento;
-    }
-
-    public void setIdTratamiento(Integer idTratamiento) {
-        IdTratamiento = idTratamiento;
     }
     public TipoTratamiento getTipo() {
         return tipo;
