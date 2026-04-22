da7427e3c0e0f1ec398a08210051fae5b8dccabb
unknown <afitz8885@gmail.com>
2026-04-22 09:42:24 -0500

    Modify Medic DAO

--- Meditrack_dao/src/main/java/pe/edu/pucp/kirusmile/dao/impl/MedicoDAOImpl.java
+++ Meditrack_dao/src/main/java/pe/edu/pucp/kirusmile/dao/impl/MedicoDAOImpl.java
@@ -7,23 +7,22 @@ import pe.edu.pucp.kirusmile.dbmanager.DBManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.SQLException;
 
 public class MedicoDAOImpl implements MedicoDAO {
 
     @Override
-    public Medico load(Integer id) {
-        String sql = "SELECT dni, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefono, correo, id, cmp, rne, fechaIngreso, firmaDigital, desactivado FROM Medico WHERE id = ? AND desactivado = 0";
+    public Medico load(String cmp) {
+        String sql = "SELECT dni, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefono, correo, cmp, rne, fechaIngreso, firmaDigital, desactivado FROM Medico WHERE cmp = ? AND desactivado = 0";
         try (Connection con = DBManager.getInstance().getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setInt(1, id);
+            ps.setString(1, cmp);
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
                     Medico medico = new Medico(
                             rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
-                            rs.getDate(5), rs.getString(6), rs.getString(7), rs.getInt(8),
-                            rs.getString(9), rs.getString(10), null, rs.getDate(11), rs.getString(12), null, rs.getBoolean(13)
+                            rs.getDate(5), rs.getString(6), rs.getString(7), rs.getString(8),
+                            rs.getString(9), null, rs.getDate(10), rs.getString(11), null, rs.getBoolean(12)
                     );
                     return medico;
                 }
@@ -39,19 +38,27 @@ public class MedicoDAOImpl implements MedicoDAO {
         t.setDesactivado(false);
         String sql = "INSERT INTO Medico (dni, nombres, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefono, correo, cmp, rne, fechaIngreso, firmaDigital, desactivado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try (Connection con = DBManager.getInstance().getConnection();
-             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            ps.setString(1, t.getDni()); ps.setString(2, t.getNombres()); ps.setString(3, t.getApellidoPaterno()); ps.setString(4, t.getApellidoMaterno());
-            if (t.getFechaNacimiento() != null) ps.setDate(5, new java.sql.Date(t.getFechaNacimiento().getTime()));
-            else ps.setNull(5, java.sql.Types.DATE);
-            ps.setString(6, t.getTelefono()); ps.setString(7, t.getCorreo()); ps.setString(8, t.getCmp()); ps.setString(9, t.getRne());
-            if (t.getFechaIngreso() != null) ps.setDate(10, new java.sql.Date(t.getFechaIngreso().getTime()));
-            else ps.setNull(10, java.sql.Types.DATE);
-            ps.setString(11, t.getFirmaDigital()); ps.setBoolean(12, t.getDesactivado());
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, t.getDni());
+            ps.setString(2, t.getNombres());
+            ps.setString(3, t.getApellidoPaterno());
+            ps.setString(4, t.getApellidoMaterno());
+            if (t.getFechaNacimiento() != null)
+                ps.setDate(5, new java.sql.Date(t.getFechaNacimiento().getTime()));
+            else
+                ps.setNull(5, java.sql.Types.DATE);
+            ps.setString(6, t.getTelefono());
+            ps.setString(7, t.getCorreo());
+            ps.setString(8, t.getCmp());
+            ps.setString(9, t.getRne());
+            if (t.getFechaIngreso() != null)
+                ps.setDate(10, new java.sql.Date(t.getFechaIngreso().getTime()));
+            else
+                ps.setNull(10, java.sql.Types.DATE);
+            ps.setString(11, t.getFirmaDigital());
+            ps.setBoolean(12, t.getDesactivado());
             ps.executeUpdate();
-            try (ResultSet rs = ps.getGeneratedKeys()) {
-                if (rs.next()) t.setId(rs.getInt(1));
-            }
-            return t; 
+            return t;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -60,16 +67,27 @@ public class MedicoDAOImpl implements MedicoDAO {
 
     @Override
     public Medico update(Medico t) {
-        String sql = "UPDATE Medico SET dni=?, nombres=?, apellidoPaterno=?, apellidoMaterno=?, fechaNacimiento=?, telefono=?, correo=?, cmp=?, rne=?, fechaIngreso=?, firmaDigital=?, desactivado=? WHERE id=?";
+        String sql = "UPDATE Medico SET dni=?, nombres=?, apellidoPaterno=?, apellidoMaterno=?, fechaNacimiento=?, telefono=?, correo=?, rne=?, fechaIngreso=?, firmaDigital=?, desactivado=? WHERE cmp=?";
         try (Connection con = DBManager.getInstance().getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setString(1, t.getDni()); ps.setString(2, t.getNombres()); ps.setString(3, t.getApellidoPaterno()); ps.setString(4, t.getApellidoMaterno());
-            if (t.getFechaNacimiento() != null) ps.setDate(5, new java.sql.Date(t.getFechaNacimiento().getTime()));
-            else ps.setNull(5, java.sql.Types.DATE);
-            ps.setString(6, t.getTelefono()); ps.setString(7, t.getCorreo()); ps.setString(8, t.getCmp()); ps.setString(9, t.getRne());
-            if (t.getFechaIngreso() != null) ps.setDate(10, new java.sql.Date(t.getFechaIngreso().getTime()));
-            else ps.setNull(10, java.sql.Types.DATE);
-            ps.setString(11, t.getFirmaDigital()); ps.setBoolean(12, t.getDesactivado()); ps.setInt(13, t.getId());
+            ps.setString(1, t.getDni());
+            ps.setString(2, t.getNombres());
+            ps.setString(3, t.getApellidoPaterno());
+            ps.setString(4, t.getApellidoMaterno());
+            if (t.getFechaNacimiento() != null)
+                ps.setDate(5, new java.sql.Date(t.getFechaNacimiento().getTime()));
+            else
+                ps.setNull(5, java.sql.Types.DATE);
+            ps.setString(6, t.getTelefono());
+            ps.setString(7, t.getCorreo());
+            ps.setString(8, t.getRne());
+            if (t.getFechaIngreso() != null)
+                ps.setDate(9, new java.sql.Date(t.getFechaIngreso().getTime()));
+            else
+                ps.setNull(9, java.sql.Types.DATE);
+            ps.setString(10, t.getFirmaDigital());
+            ps.setBoolean(11, t.getDesactivado());
+            ps.setString(12, t.getCmp());
             ps.executeUpdate();
             return t;
         } catch (SQLException e) {
@@ -81,11 +99,11 @@ public class MedicoDAOImpl implements MedicoDAO {
     @Override
     public void remove(Medico t) {
         t.setDesactivado(true);
-        String sql = "UPDATE Medico SET desactivado = ? WHERE id = ?";
+        String sql = "UPDATE Medico SET desactivado = ? WHERE cmp = ?";
         try (Connection con = DBManager.getInstance().getConnection();
              PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setBoolean(1, t.getDesactivado());
-            ps.setInt(2, t.getId());
+            ps.setString(2, t.getCmp());
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
