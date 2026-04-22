55435db3f839c127db9da8de1c634fcf32d19b9c
Jair LLaiqui <a20222757@pucp.edu.pe>
2026-04-22 07:16:33 -0500

    Update Program.java

--- AppPrueba/src/main/java/Program.java
+++ AppPrueba/src/main/java/Program.java
@@ -10,7 +10,7 @@ public class Program {
         System.out.println("--- INICIANDO PRUEBAS CRUD ---");
 
         // Inicializar DAOs
-        PacienteDAO pacienteDAO = new PacienteDAOImpl();
+        EspecialidadDAO especialidadDAO = new EspecialidadDAOImpl();
         MedicoDAO medicoDAO = new MedicoDAOImpl();
         CitaMedicaDAO citaMedicaDAO = new CitaMedicaDAOImpl();
         TratamientoDAO tratamientoDAO = new TratamientoDAOImpl();
@@ -18,53 +18,48 @@ public class Program {
         // ---------------------------------------------------------
         // 1. CREACIÓN DE DATOS (SAVE)
         // ---------------------------------------------------------
-        System.out.println("\n1. Insertando Paciente...");
-        Paciente p1 = new Paciente("12345678", "Juan", "Perez", "Gomez", new java.util.Date(), 
-                                   "999888777", "juan@gmail.com", 1, "ACTIVO", true, false);
-        pacienteDAO.save(p1);
-        System.out.println("  -> Paciente guardado con ID autogenerado: " + p1.getId());
-
+        System.out.println("\n1. Insertando Especialidad...");
+        Especialidad esp = new Especialidad(2, "Endocrinologia", 70.0,true);
+        especialidadDAO.save(esp);
+        System.out.println("  -> Especialidad guardado con ID autogenerado: " + esp.getIdEspecialidad());
+/*
         System.out.println("\n2. Insertando Medico...");
         Medico m1 = new Medico("87654321", "Dr. Luis", "Sanchez", "Vera", new java.util.Date(), 
                                "999111222", "luis@med.com", 2, "CMP123", "RNE456", null, new java.util.Date(), 
                                "firma_digital_xyz", null, false);
         medicoDAO.save(m1);
         System.out.println("  -> Medico guardado con ID autogenerado: " + m1.getId());
-
-        System.out.println("\n3. Insertando Cita Medica...");
-        CitaMedica c1 = new CitaMedica(3, new java.util.Date(), LocalTime.of(10, 0), LocalTime.of(10, 30), p1, m1, "PROGRAMADA", false);
-        citaMedicaDAO.save(c1);
-        System.out.println("  -> Cita guardada con ID BD: " + c1.getIdCita());
-
+*/
+/*
         System.out.println("\n4. Insertando Tratamiento...");
         Tratamiento t1 = new Tratamiento(4, TipoTratamiento.PREVENTIVO, "Tomar paracetamol", new java.util.Date(), new java.util.Date(), false);
         tratamientoDAO.save(t1);
         System.out.println("  -> Tratamiento guardado (ID): " + t1.getIdTratamiento());
-
+*/
 
         // ---------------------------------------------------------
         // 2. LECTURA DE DATOS (LOAD)
         // ---------------------------------------------------------
         System.out.println("\n--- PRUEBA DE LECTURA (LOAD) usando el nuevo Integer ID ---");
         // Nota: Si la bd está vacía, p1.getId() tendría el primer ID (ej: 1)
-        Integer idPacienteABuscar = p1.getId() != null ? p1.getId() : 1; 
-        Paciente pLoaded = pacienteDAO.load(idPacienteABuscar);
-        if(pLoaded != null) System.out.println("Paciente leido BD -> ID: " + pLoaded.getId() + " | Nombres: " + pLoaded.getNombres());
-        
+        Integer idEspecialidadABuscar = esp.getIdEspecialidad() != -1 ? esp.getIdEspecialidad() : 1;
+        Especialidad pLoaded = especialidadDAO.load(idEspecialidadABuscar);
+        if(pLoaded != null) System.out.println("Especialidad leida BD -> ID: " + pLoaded.getIdEspecialidad() + " | Nombre: " + pLoaded.getNombreEspecialidad());
+/*
         Integer idMedicoABuscar = m1.getId() != null ? m1.getId() : 1;
         Medico mLoaded = medicoDAO.load(idMedicoABuscar);
         if(mLoaded != null) System.out.println("Medico leido BD -> ID: " + mLoaded.getId() + " | CMP: " + mLoaded.getCmp());
-        
+ */
 
         // ---------------------------------------------------------
         // 3. ACTUALIZACION DE DATOS (UPDATE)
         // ---------------------------------------------------------
         System.out.println("\n--- PRUEBA DE ACTUALIZACION (UPDATE) ---");
         if (pLoaded != null) {
-            System.out.println("Correo anterior: " + pLoaded.getCorreo());
-            pLoaded.setCorreo("juan_nuevo_correo@hotmail.com");
-            pacienteDAO.update(pLoaded);
-            System.out.println("  -> Correo de Paciente actualizado en BD a: " + pLoaded.getCorreo());
+            System.out.println("Nombre anterior: " + pLoaded.getNombreEspecialidad());
+            pLoaded.setNombreEspecialidad("Dentista");
+            especialidadDAO.update(pLoaded);
+            System.out.println("  -> Nombre de especialidad actualizado en BD a: " + pLoaded.getNombreEspecialidad());
         }
         
         // ---------------------------------------------------------
@@ -72,7 +67,7 @@ public class Program {
         // ---------------------------------------------------------
         System.out.println("\n--- PRUEBA DE ELIMINACION (Soft Delete) ---");
         if (pLoaded != null) {
-            pacienteDAO.remove(pLoaded);
+            especialidadDAO.remove(pLoaded);
             System.out.println("  -> Correcto: Paciente desactivado lógicamente (desactivado = true)");
         }
 
